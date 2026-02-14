b0cd5e663ff674b149333fbc6fd43b72461801ea
mokatimojalefa781-cell <mokatimojalefa781@gmail.com>
2026-02-14 21:31:55 +0200

    Delete news directory

--- news/api_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .api_views import ReaderSubscribedArticlesAPIView
-
-app_name = "news_api"
-urlpatterns = [
-    path("subscribed-articles/", ReaderSubscribedArticlesAPIView.as_view(), name="reader_articles"),
-]
